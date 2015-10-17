983d7b2721f7f482da1a365e96f2f7b6de19afdf
rilcy <cyril.zufferey@outlook.com>
2015-10-17 19:36:08 +0200

    f

--- app/src/main/java/devmobile/tvshow/MainActivity.java
+++ app/src/main/java/devmobile/tvshow/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,21 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] serieName = {"NCIS : Los Angeles", "Game of Thrones", "Impastor", "Person of Interest", "Dexter",
-                                    "The Americans", "Dardevil"};
+       final ArrayList<Serie> listOfSeries = new ArrayList<Serie>();
 
-        String[] serieRepositoryImg = {"","","","","","",""};
 
-        ListAdapter adapter = new CustomAdapter(this, serieName);
-        final ListView listOfSeries = (ListView) findViewById(R.id.listOfSeries);
-        listOfSeries.setAdapter(adapter);
+        Serie ncis = new Serie("NCIS : Los Angeles", R.drawable.ncis_la);
+        listOfSeries.add(ncis);
+        Serie got = new Serie("Game of Thrones", R.drawable.got);
+        listOfSeries.add(got);
+        Serie impastor = new Serie("Impastor", R.drawable.impastor);
+        listOfSeries.add(impastor);
+        Serie poi = new Serie("Person of Interest", R.drawable.person_of_interest);
+        listOfSeries.add(poi);
+        Serie dexter = new Serie("Dexter", R.drawable.dexter);
+        listOfSeries.add(dexter);
+        Serie americans = new Serie("The Americans", R.drawable.the_americans);
+        listOfSeries.add(americans);
+        Serie daredevil = new Serie("Daredevil", R.drawable.daredevil);
+        listOfSeries.add(daredevil);
 
-        listOfSeries.setOnItemClickListener(
+
+        ListAdapter adapter = new CustomAdapter(this, listOfSeries);
+
+        ListView list = (ListView) findViewById(R.id.listOfSeries);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String serie = String.valueOf(parent.getItemAtPosition(position));
-                        Toast.makeText(MainActivity.this, serie, LENGTH_LONG).show();
+                        Serie serie = listOfSeries.get(position);
+                        Toast.makeText(MainActivity.this, serie.getTitle(), LENGTH_LONG).show();
                     }
                 }
         );
