14469a52e9577605d108ab70729947ccb7766476
rilcy <cyril.zufferey@outlook.com>
2015-10-29 19:56:59 +0100

    f

---
+++ app/src/main/java/devmobile/tvshow/CustomAdapterNextToWatch.java
@@ -0,0 +1,47 @@
+package devmobile.tvshow;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rilcy on 29.10.15.
+ */
+public class CustomAdapterNextToWatch extends ArrayAdapter<Episode>{
+
+    private Episode episode;
+
+    public CustomAdapterNextToWatch(Context context, ArrayList<Episode> episode) {
+        super(context, R.layout.custom_row, (ArrayList) episode);
+
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View customView = inflater.inflate(R.layout.custom_row, parent, false);
+
+        Episode episode = getItem(position);
+        TextView textViewTitle = (TextView) customView.findViewById(R.id.serieTitle);
+        TextView textViewInfo = (TextView) customView.findViewById(R.id.serieInfo);
+        ImageView imageView = (ImageView) customView.findViewById(R.id.imageSerie);
+
+        textViewTitle.setText(episode.getEpisodeTitle());
+        textViewInfo.setText(episode.getEpisodeInfo());
+        imageView.setImageResource(episode.getImg());
+
+        // SRC POUR AJOUTER L'IMAGE : http://www.androidinterview.com/android-custom-listview-with-image-and-text-using-arrayadapter/
+        // https://youtu.be/U_Jvk4G28YE New Boston
+
+        return customView;
+    }
+}
