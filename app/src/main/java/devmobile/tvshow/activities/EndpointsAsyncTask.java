13f9e20b64cd6bd5a7984f625aaaaf564799a467
Elsio <elsio.coelho@gmail.com>
2016-01-10 15:36:26 +0100

    Cela avance : backend

--- app/src/main/java/devmobile/tvshow/activities/EndpointsAsyncTask.java
+++ app/src/main/java/devmobile/tvshow/activities/EndpointsAsyncTask.java
@@ -25,6 +25,7 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
+                    //.setRootUrl("https://tvshow-1186.appspot.com/")
                     .setRootUrl("http://10.0.2.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
